ad6273a5896873c3c12a9edca588ebb5647a5967
GoncaloSaraiva07 <borges.saraiva07@gmail.com>
2026-06-30 23:34:40 +0000

    Alterar logo 3

--- scouting_dashboard/app_scouting_dashboard.py
+++ scouting_dashboard/app_scouting_dashboard.py
@@ -91,8 +91,8 @@ def render_dashboard_header():
     else:
         logo_html = f"""
         <img src="{logo_base64}" style="
-            width: 178px;
-            max-height: 109px;
+            width: 165px;
+            max-height: 101px;
             object-fit: contain;
             display: block;
         ">
